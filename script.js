b6d8e10d822465d1d6cdf138e1524d011e39a36c
Pachara Buatim <johneilish@MacBook-Air.local>
2026-08-15 11:04:14 +0700

    Using class instead of old constructor

--- script.js
+++ script.js
@@ -46,12 +46,14 @@ document.addEventListener("click", () => {
 
 const library = [];
 
-function Book(title, author, pages, status) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+class Book {
+  constructor(title, author, pages, status) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
 }
 
 function showBook(book) {
